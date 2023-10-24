ab10f9063eb45bd0a27c3bb1e3213352df8f1cdb
Martin Langgaard Jacobsen <martin.l.jacobsen2010@live.dk>
2023-10-24 09:22:34 +0200

    small changes

--- src/service/mealPlanManager.py
+++ src/service/mealPlanManager.py
@@ -22,13 +22,13 @@ def getRecipeFromInventory(userid: int, calorieTarget: int, proteinTarget: int, 
     recipe = requests.post(url, calorieTarget, proteinTarget, carbTarget, fatTarget, food, mealTypes, mealsPerDay)
     
 
-def splitCaloriesAndMacros(userData[0]):
-    getUserTargets
-    url = f"api/getUserInfo/macros"
+# def splitCaloriesAndMacros(userData[0]):
+#     getUserTargets
+#     url = f"api/getUserInfo/macros"
     
-    breakfastTarget = [calorieTarget*0.2, proteinTarget*0.2, carbTarget*0.2, fatTarget*0.2]
-    lunchTarget = [calorieTarget*0.3, proteinTarget*0.3, carbTarget*0.3, fatTarget*0.3]
-    dinnerTarget = [calorieTarget*0.5, proteinTarget*0.5, carbTarget*0.5, fatTarget*0.5]
+#     breakfastTarget = [calorieTarget*0.2, proteinTarget*0.2, carbTarget*0.2, fatTarget*0.2]
+#     lunchTarget = [calorieTarget*0.3, proteinTarget*0.3, carbTarget*0.3, fatTarget*0.3]
+#     dinnerTarget = [calorieTarget*0.5, proteinTarget*0.5, carbTarget*0.5, fatTarget*0.5]
     
 
 # CalorieTarget: int
