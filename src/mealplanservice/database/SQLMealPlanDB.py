198fbb4ab7278247a5ae5a74109e36d405ca0cf9
Martin Langgaard Jacobsen <martin.l.jacobsen2010@live.dk>
2023-11-01 11:16:06 +0100

    gitignore and SQLMealPlanDB changes
    .gitignore works flawlessly. We also changed up some stuff in SQLMealPlanDB, using a join and trying a dictionary

--- src/mealplanservice/database/SQLMealPlanDB.py
+++ src/mealplanservice/database/SQLMealPlanDB.py
@@ -44,94 +44,119 @@ class SQLMealPlanDB(BaseMealPlanDB):
         self.execute_query("DELETE FROM mealPlan WHERE planID=%s", (planID,))
         return {"Message": "Success", "planID": planID}
 
+    # def get_current_meal_plan(self, userID: int):
+    #     self.execute_query("SELECT * FROM mealPlan WHERE userID=%s ORDER BY planID DESC", (userID,))
+    #     meal_plan_result   = self.__cursor.fetchone()
+    #     meal_plan_json = {
+    #         "planID": meal_plan_result[0],
+    #         "userID": meal_plan_result[1],
+    #         "startDate": str(meal_plan_result[2]),
+    #         "endDate": str(meal_plan_result[3]),
+    #         "totalCalories": meal_plan_result[4],
+    #         "totalProtein": meal_plan_result[5],
+    #         "totalCarbohydrates": meal_plan_result[6],
+    #         "totalFat": meal_plan_result[7]
+    #     }
 
+    #     self.execute_query("SELECT id, recipeID FROM mealplanrecipes WHERE planID=%s ORDER BY id DESC", (meal_plan_json["planID"],))
+    #     meal_plan_recipes_result = self.__cursor.fetchall()
+    #     recipe_id_list = []
+    #     for row in meal_plan_recipes_result:
+    #         recipe_id_list.append(row[1])
+
+    #     self.execute_query("SELECT id, meals FROM mealsperday WHERE planID=%s ORDER BY id DESC", (meal_plan_json["planID"],))
+    #     meals_per_day_result = self.__cursor.fetchall()
+    #     meals_per_day_list = []
+    #     for row in meals_per_day_result:
+    #         meals_per_day_list.append(row[1])
+
+    #     days = []
+    #     latest_recipe_index = 0
+    #     for day in range(0, len(meals_per_day_list)):
+    #         day_content = {}
+    #         for meal in range(meals_per_day_list[day]):
+    #             day_content[f"recipeID{meal+1}"] = recipe_id_list[latest_recipe_index + meal]
+    #         days.append(day_content)
+    #         latest_recipe_index += meal + 1
+    #     days.reverse()
+    #     meal_plan_json["days"] = days
+
+    #     return meal_plan_json
 
     def get_current_meal_plan(self, userID: int):
-        self.execute_query("SELECT * FROM mealPlan WHERE userID=%s ORDER BY planID DESC", (userID,))
-        meal_plan_result   = self.__cursor.fetchone()
-        planID             = meal_plan_result[0]
-        userID             = meal_plan_result[1]
-        startDate          = meal_plan_result[2]
-        endDate            = meal_plan_result[3]
-        totalCalories      = meal_plan_result[4]
-        totalProtein       = meal_plan_result[5]
-        totalCarbohydrates = meal_plan_result[6]
-        totalFat           = meal_plan_result[7]
+        self.execute_query("""
+            SELECT mp.*, mpr.recipeID, mpd.meals
+            FROM mealPlan mp
+            LEFT JOIN mealplanrecipes mpr ON mp.planID = mpr.planID
+            LEFT JOIN mealsperday mpd ON mp.planID = mpd.planID
+            WHERE mp.userID=%s
+            ORDER BY mp.planID DESC
+        """, (userID,))
 
-        self.execute_query("SELECT id, recipeID FROM mealplanrecipes WHERE planID=%s ORDER BY id DESC", (planID,))
-        meal_plan_recipes_result = self.__cursor.fetchall()
-        recipe_id_list = []
-        for row in meal_plan_recipes_result:
-            recipe_id_list.append(row[1])
+        meal_plan_results = self.__cursor.fetchall()
 
-        self.execute_query("SELECT id, meals FROM mealsperday WHERE planID=%s ORDER BY id DESC", (planID,))
-        meals_per_day_result = self.__cursor.fetchall()
-        meals_per_day_list = []
-        for row in meals_per_day_result:
-            meals_per_day_list.append(row[1])
+        if not meal_plan_results:
+            return None
 
         meal_plan_json = {
-            "planID": planID,
-            "userID": userID,
-            "startDate": str(startDate),
-            "endDate": str(endDate),
-            "totalCalories": totalCalories,
-            "totalProtein": totalProtein,
-            "totalCarbohydrates": totalCarbohydrates,
-            "totalFat": totalFat
+            "planID": meal_plan_results[0][0],
+            "userID": meal_plan_results[0][1],
+            "startDate": str(meal_plan_results[0][2]),
+            "endDate": str(meal_plan_results[0][3]),
+            "totalCalories": meal_plan_results[0][4],
+            "totalProtein": meal_plan_results[0][5],
+            "totalCarbohydrates": meal_plan_results[0][6],
+            "totalFat": meal_plan_results[0][7],
+            "days": []
         }
 
         days = []
+        day_content = {}
         latest_recipe_index = 0
-        for day in range(0, len(meals_per_day_list)):
-            day_content = {}
-            for meal in range(meals_per_day_list[day]):
-                day_content[f"recipeID{meal+1}"] = recipe_id_list[latest_recipe_index + meal]
-            days.append(day_content)
-            latest_recipe_index += meal + 1
+
+        for row in meal_plan_results:
+            if row[8] is not None:  # Check if there is a recipeID
+                day_content[f"recipeID{len(day_content) + 1}"] = row[8]
+
+            if len(day_content) == row[9]:  # Check if we have added all meals for the day
+                days.append(day_content)
+                day_content = {}
+                latest_recipe_index += row[9]
+
         days.reverse()
         meal_plan_json["days"] = days
 
         return meal_plan_json
+
     
     def get_all_meal_plans(self, userID: int):
         self.execute_query("SELECT * FROM mealPlan WHERE userID=%s ORDER BY planID DESC", (userID,))
-        meal_plan_result1   = self.__cursor.fetchall()
+        meal_plan_results   = self.__cursor.fetchall()
         plan_num = 1
         meal_plan_json = {}
-        for meal_plan_result in meal_plan_result1:
-            planID             = meal_plan_result[0]
-            userID             = meal_plan_result[1]
-            startDate          = meal_plan_result[2]
-            endDate            = meal_plan_result[3]
-            totalCalories      = meal_plan_result[4]
-            totalProtein       = meal_plan_result[5]
-            totalCarbohydrates = meal_plan_result[6]
-            totalFat           = meal_plan_result[7]
+        for meal_plan_result in meal_plan_results:
+            meal_plan_json[f"plan{plan_num}"] = {
+                "planID": meal_plan_result[0],
+                "userID": meal_plan_result[1],
+                "startDate": str(meal_plan_result[2]),
+                "endDate": str(meal_plan_result[3]),
+                "totalCalories": meal_plan_result[4],
+                "totalProtein": meal_plan_result[5],
+                "totalCarbohydrates": meal_plan_result[6],
+                "totalFat": meal_plan_result[7]
+                }
 
-            self.execute_query("SELECT id, recipeID FROM mealplanrecipes WHERE planID=%s ORDER BY id DESC", (planID,))
+            self.execute_query("SELECT id, recipeID FROM mealplanrecipes WHERE planID=%s ORDER BY id DESC", (meal_plan_json[f"plan{plan_num}"]["planID"],))
             meal_plan_recipes_result = self.__cursor.fetchall()
             recipe_id_list = []
             for row in meal_plan_recipes_result:
                 recipe_id_list.append(row[1])
 
-            self.execute_query("SELECT id, meals FROM mealsperday WHERE planID=%s ORDER BY id DESC", (planID,))
+            self.execute_query("SELECT id, meals FROM mealsperday WHERE planID=%s ORDER BY id DESC", (meal_plan_json[f"plan{plan_num}"]["planID"],))
             meals_per_day_result = self.__cursor.fetchall()
             meals_per_day_list = []
             for row in meals_per_day_result:
-                meals_per_day_list.append(row[1])
-        
-            meal_plan_json[f"plan{plan_num}"] = {
-                "planID": planID,
-                "userID": userID,
-                "startDate": str(startDate),
-                "endDate": str(endDate),
-                "totalCalories": totalCalories,
-                "totalProtein": totalProtein,
-                "totalCarbohydrates": totalCarbohydrates,
-                "totalFat": totalFat
-                }
-            
+                meals_per_day_list.append(row[1])            
 
             days = []
             latest_recipe_index = 0
