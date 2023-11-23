89d3c30feca0fe8f2b02038900f164044e9b4ec9
Kristoffer Gregersen <33570390+Kristoffermg@users.noreply.github.com>
2023-11-23 11:13:42 +0100

    Tests + configuration (#13)
    * Current attempt at generating a mealplan
    
    * Simple mealplan generation
    
    * Can now generate a parameterized mealplan
    
    Updated generate_meal_plan to now be able to generate a mealplan through specified parameters.
    
    Cleaned up in SQLMealPlanDB.py
    Updated README.md to include new information.
    
    * FInal clean-up
    
    * Can now generate mealplan and insert into database
    
    Meelplan is now generated and inserted into database
    
    Database schema is updated
    
    fixed functions after updated database
    
    * Rename baseMealPlanDB.py to BaseMealPlanDB.py
    
    * Implemented assert_has_calls tests
    
    * Fixed minor errors
    
    * fixed merge and added config class
    
    * Delete mealPlanTests directory
    
    * fix tests
    
    ---------
    
    Co-authored-by: Martin Langgaard Jacobsen <martin.l.jacobsen2010@live.dk>
    Co-authored-by: Martin Langgaard Jacobsen <72913182+Flaiizor@users.noreply.github.com>

--- app/server.py
+++ app/server.py
@@ -3,9 +3,21 @@ from fastapi import FastAPI
 from mealplanservice.database import SQLMealPlanDB
 from mealplanservice import mealPlanService
 import os
+from dotenv import dotenv_values
 
+class Config:
+    def __init__(self) -> None:
+        self.__env = os.environ
+        self.__envfile = dict()
+        if os.path.exists(".env"):
+            self.__envfile = dotenv_values(".env")
 
-cfg = os.environ
+    def __getitem__(self, key: str):
+        if key in self.__env:
+            return self.__env[key]
+        return self.__envfile[key]
+
+cfg = Config()
 
 app = FastAPI()
 db = SQLMealPlanDB(cfg)
